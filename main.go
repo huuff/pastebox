0d51f29b8dd1c74f9ec83c3694a6a762a7122a98
Haf <haf@protonmail.ch>
2023-08-15 11:50:49 +0200

    refactor to remove any traces of snippetbox and added non-prefix /

--- main.go
+++ main.go
@@ -6,23 +6,28 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+  if r.URL.Path != "/" {
+    http.NotFound(w, r)
+    return
+  }
+
   w.Write([]byte("Hello from Pastebox"))
 }
 
-func snippetView(w http.ResponseWriter, r *http.Request) {
+func pasteView(w http.ResponseWriter, r *http.Request) {
   w.Write([]byte("Display a specific paste"))
 }
 
-func snippetCreate(w http.ResponseWriter, r *http.Request) {
-  w.Write([]byte("Create a new snippet"))
+func pasteCreate(w http.ResponseWriter, r *http.Request) {
+  w.Write([]byte("Create a new paste"))
 }
 
 func main() {
   mux := http.NewServeMux()
 
   mux.HandleFunc("/", home)
-  mux.HandleFunc("/snippet/view", snippetView)
-  mux.HandleFunc("/snippet/create", snippetCreate)
+  mux.HandleFunc("/paste/view", pasteView)
+  mux.HandleFunc("/paste/create", pasteCreate)
 
   log.Println("Starting server on :4000")
   err := http.ListenAndServe(":4000", mux)
