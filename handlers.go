836f9e6b47c03a991c6617597c251ea22b5c9420
Haf <haf@protonmail.ch>
2023-08-16 06:13:48 +0200

    project reorganization

---
+++ handlers.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+  "net/http"
+  "strconv"
+  "fmt"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+  if r.URL.Path != "/" {
+    http.NotFound(w, r)
+    return
+  }
+
+  w.Write([]byte("Hello from Pastebox"))
+}
+
+func pasteView(w http.ResponseWriter, r *http.Request) {
+
+  id, err := strconv.Atoi(r.URL.Query().Get("id"))
+  if err != nil || id < 1 {
+    http.NotFound(w, r)
+    return
+  }
+
+  fmt.Fprintf(w, "Display a specific paste with id %d...", id)
+}
+
+func pasteCreate(w http.ResponseWriter, r *http.Request) {
+  if r.Method != http.MethodPost {
+    w.Header().Set("Allow", http.MethodPost)
+    http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+    return
+  }
+
+  w.Write([]byte("Create a new paste"))
+}
