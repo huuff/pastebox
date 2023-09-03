ec69ec8b4a250c3200f22e50d47f8eda7ccadcaa
Haf <haf@protonmail.ch>
2023-09-03 15:29:26 +0200

    recovering from panics with middleware

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -13,5 +13,5 @@ func (app *application) routes() http.Handler {
   mux.HandleFunc("/paste/view", app.pasteView)
   mux.HandleFunc("/paste/create", app.pasteCreate)
 
-  return app.logRequest(secureHeaders(mux))
+  return app.recoverPanic(app.logRequest(secureHeaders(mux)))
 }
