1c68f76978fb0734302b4bcd025a9d13f98ce555
Haf <haf@protonmail.ch>
2023-09-03 15:22:52 +0200

    secure headers middleware

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -2,7 +2,7 @@ package main
 
 import "net/http"
 
-func (app *application) routes() *http.ServeMux {
+func (app *application) routes() http.Handler {
   mux := http.NewServeMux()
 
   
@@ -13,5 +13,5 @@ func (app *application) routes() *http.ServeMux {
   mux.HandleFunc("/paste/view", app.pasteView)
   mux.HandleFunc("/paste/create", app.pasteCreate)
 
-  return mux
+  return secureHeaders(mux)
 }
