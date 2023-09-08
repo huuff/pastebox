86c82fc353b77df1ac37406b91d0ffff73b04990
Haf <haf@protonmail.ch>
2023-09-08 05:55:21 +0200

    re-filling form and showing errors

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -57,8 +57,18 @@ func (app *application) pasteView(w http.ResponseWriter, r *http.Request) {
 
 func (app *application) pasteCreate(w http.ResponseWriter, r *http.Request) {
   data := app.newTemplateData(r)
+  data.Form = pasteCreateForm {
+    Expires: 365,
+  }
 
   app.render(w, http.StatusOK, "create.gotmpl", data)
+}
+
+type pasteCreateForm struct {
+  Title string
+  Content string
+  Expires int
+  FieldErrors map[string]string
 }
 
 func (app *application) pasteCreatePost(w http.ResponseWriter, r *http.Request) {
@@ -69,38 +79,44 @@ func (app *application) pasteCreatePost(w http.ResponseWriter, r *http.Request) 
     return
   }
 
-  title := r.PostForm.Get("title")
-  content := r.PostForm.Get("content")
   expires, err := strconv.Atoi(r.PostForm.Get("expires"))
   if err != nil {
     app.clientError(w, http.StatusBadRequest)
     return
   }
-
-  app.infoLog.Printf("Creating paste '%s': '%s'. Expires in %d days", title, content, expires)
-
-  fieldErrors := make(map[string]string)
-
-  if strings.TrimSpace(title) == "" {
-    fieldErrors["title"] = "This field cannot be blank"
-  } else if utf8.RuneCountInString(title) > 100 {
-    fieldErrors["title"] = "This field cannot be over 100 characters long"
+  
+  form := pasteCreateForm {
+    Title: r.PostForm.Get("title"),
+    Content: r.PostForm.Get("content"),
+    Expires: expires,
+    FieldErrors: map[string]string{},
   }
 
-  if strings.TrimSpace(content) == "" {
-    fieldErrors["content"] = "This field cannot be blank"
+  app.infoLog.Printf("Creating paste '%s': '%s'. Expires in %d days", form.Title, form.Content, expires)
+
+
+  if strings.TrimSpace(form.Title) == "" {
+    form.FieldErrors["title"] = "This field cannot be blank"
+  } else if utf8.RuneCountInString(form.Title) > 100 {
+    form.FieldErrors["title"] = "This field cannot be over 100 characters long"
+  }
+
+  if strings.TrimSpace(form.Content) == "" {
+    form.FieldErrors["content"] = "This field cannot be blank"
   }
 
   if expires != 1 && expires != 7 && expires != 365 {
-    fieldErrors["expires"] = "This field must be 1, 7, or 365"
+    form.FieldErrors["expires"] = "This field must be 1, 7, or 365"
   }
 
-  if len(fieldErrors) != 0 {
-    fmt.Fprint(w, fieldErrors)
+  if len(form.FieldErrors) != 0 {
+    data := app.newTemplateData(r)
+    data.Form = form
+    app.render(w, http.StatusUnprocessableEntity, "create.gotmpl", data)
     return
   }
 
-  id, err := app.pastes.Insert(title, content, expires)
+  id, err := app.pastes.Insert(form.Title, form.Content, expires)
   if err != nil {
     app.serverError(w, err)
     return
