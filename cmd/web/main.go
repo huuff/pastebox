41152604e16a7db5e5a0835d3fdc838c8b248250
Haf <haf@protonmail.ch>
2023-09-13 05:54:23 +0200

    some timeouts

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
   "github.com/gookit/validate"
+  "time"
 )
 
 
@@ -17,6 +18,9 @@ func main() {
     Addr: args.Addr(),
     ErrorLog: app.errorLog,
     Handler: app.routes(),
+    IdleTimeout: time.Minute,
+    ReadTimeout: 5 * time.Second,
+    WriteTimeout: 10 * time.Second,
   }
 
   validate.Config(func(opt *validate.GlobalOption) {
